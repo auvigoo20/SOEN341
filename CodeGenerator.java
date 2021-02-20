9879578730b0daeb248a996c05f4a8ddeb5be0b0
david-gitxyz <79176006+david-gitxyz@users.noreply.github.com>
2021-02-19 19:48:29 -0500

    Update CodeGenerator.java
    Friday, February 19, 2021

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -1,10 +1,58 @@
-// step 7:  Jay, Vithu, David
+// step 7:  Jay, Vithu, David 
+
+//don't know if we're allowed to use printwriter
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 
 public class CodeGenerator {
-<<<<<<< Updated upstream
     //attributes
-=======
-    //Hello world;
->>>>>>> Stashed changes
+    private IntermediateRepresentation IR;
+    private Object symbolTable; //Not sure if symbolTable is object
+    //private String option;
+
+    //methods
+    /**
+     * Traverse the IR using symbol table 
+     */
+    public void traverseIR(IntermediateRepresentation IR, Object symbolTable){
+        //don't know how an IR and symbol table look like, need example from prof
+    } 
+
+    /**
+     * Generates the listing file
+     */
+   
+     public void generateListing(){
     
-}
+        //create listing file to output to
+        PrintWriter outStr = null;
+        try {
+            outStr = new PrintWriter(new FileOutputStream("listing.txt"));
+        } catch(FileNotFoundException e) {
+            System.out.println("Cannot read file!");
+        }
+
+        //Create format header
+        outStr.println("Line Addr Code Label Mne Operand Comment");
+
+        //Generating opening line statement - TO DO
+
+        //generate closing line statement - TO DO
+
+        //close listing file
+        outStr.close();
+    } 
+
+    /**
+     * Generates the executable file, to do later
+    void generateExecutable(){}
+    */  
+
+    /*
+    public static void main(String[] args){
+        CodeGenerator code = new CodeGenerator();
+        code.generateListing();
+    } 
+    */   
+} 
