2f590bc4695f34cb5b5fa7dfbfbbb09d9e186657
david-gitxyz <79176006+david-gitxyz@users.noreply.github.com>
2021-02-21 18:44:16 -0500

    Update ICGenerator.java

--- ICGenerator.java
+++ ICGenerator.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public interface ICGenerator {
     
-    public void traverseIR(ArrayList IR, SymbolTable symbolTable); //traverse IR using symbol table
+    public void traverseIR(ArrayList<LineStatement> IR, SymbolTable symbolTable); //traverse IR using symbol table
 
     public void generateListing();
 
