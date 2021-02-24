d7f3ddf24e657e40332f6fef418ded1a890cfb69
Auvigoo Ahmed <auvigooa@gmail.com>
2021-02-23 21:38:59 -0500

    Created test for Symbol Table

---
+++ TestSymbolTable.java
@@ -0,0 +1,42 @@
+import java.util.LinkedHashMap;
+public class TestSymbolTable {
+
+    public static void main(String[] args){
+
+        //Create new symbol table
+        SymbolTable symbolTable = new SymbolTable();
+
+        //Create tokens to insert into symbol table
+        Token token1 = new Token(new Instruction("not"), "\n");
+        Token token2 = new Token(new Instruction("and"), "\n");
+        Token token3 = new Token(new Instruction("or"), "\n");
+
+        //Insert tokens into symbol table
+        symbolTable.insertMnemonic("not", token1);
+        symbolTable.insertMnemonic("and", token2);
+        symbolTable.insertMnemonic("or", token3);
+
+        LinkedHashMap<String, Token> hashMap = symbolTable.gHashMap();
+
+        //Expected Output
+        System.out.println("Test Symbol Table");
+        System.out.println("not:not and:and or:or ");
+
+        //Actual Output
+        for(String mnemonic: hashMap.keySet()){
+
+            System.out.print(mnemonic+":");
+            System.out.print(symbolTable.gHashMap().get(mnemonic));
+            System.out.print(" ");
+
+        }
+        System.out.println();
+
+
+        
+        
+        
+
+    }
+    
+}
