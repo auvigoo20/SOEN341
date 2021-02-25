1bed8fa55131206dcd5319368c13bc63c615cdcd
JoeyMoarbes <78099595+JoeyMoarbes@users.noreply.github.com>
2021-02-25 13:21:43 -0500

    Update LexicalAnalyzer.java

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -3,30 +3,30 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.HashMap; //Importing all the relevant packages and libraries for use in the LexicalAnalyzer
 
 public class LexicalAnalyzer implements ILexicalAnalyzer
 {
-    private FileInputStream fis;
+    private FileInputStream fis; //Creating an object of type FileInputStream for use in opening the file.
 
 
-    public LexicalAnalyzer(){
+    public LexicalAnalyzer(){ //Creating a method for the lexicalAnalyzer, the default constructor
 
         try{
-            this.fis = new FileInputStream("TestInherentMnemonics.asm");
+            this.fis = new FileInputStream("TestInherentMnemonics.asm"); //Opening the input file specified
         }
-        catch(FileNotFoundException e){
+        catch(FileNotFoundException e){ //Checking if the file was read correctly. 
             System.out.println("File not found");
             System.exit(0);
         }
     }
 
-    public LexicalAnalyzer(String fileName){
+    public LexicalAnalyzer(String fileName){ //Creating a method for the lexicalAnalyzer, the parametrized constructor
 
         try{
-            this.fis = new FileInputStream(fileName);
+            this.fis = new FileInputStream(fileName); //Opening the input file, with fileName taken as parameter
         }
-        catch(FileNotFoundException e){
+        catch(FileNotFoundException e){ //Checking if the file was read correctly. 
             System.out.println("File not found");
             System.exit(0);
         }
@@ -35,53 +35,45 @@ public class LexicalAnalyzer implements ILexicalAnalyzer
 
 
 
-    // method to read file character by character and generate token
-   public void readFileByLine(Parser p, SymbolTable symbolTable){
+  
+   public void readFileByLine(Parser p, SymbolTable symbolTable){ //Method to read file character by character and generate token
     
     try{    
-        int i;
-        String mnemonic = "";
+        int i; //Declaring integer for use in skipping white spaces
+        String mnemonic = ""; //Declaring a mnemonic string variable for use in the method
 
 
         while((i = fis.read()) != -1){
-            // System.out.print((char)i);
-         
-               
-                //ASCII of \n == 10. If the current character is a newline, use the built string
-                if(i == 10 ){
+        
+                
+                if(i == 10 ){//ASCII of \n == 10. If the current character is a newline, use the previously defined string
                     
-                    if(mnemonic.length() > 0){
+                    if(mnemonic.length() > 0){ //If mnemonic variable was not empty
 
-                     // make checks to see if mnemonic, label, or comment, or EOL -- TODO
-                    Token t = new Token(new Instruction(mnemonic), "\n");
-                    
+                    Token t = new Token(new Instruction(mnemonic), "\n"); // Creating a token object and passing in the mnemonic variable as an argument as an instruction
 
-                    //Insert mnemonic in the symbol table. This mnemonic is the same as the one that is sent as a token.
-                    symbolTable.insertMnemonic(mnemonic, t);
-                    
-                    //Return token to the Parser
-                    p.requestToken(t);
+                    symbolTable.insertMnemonic(mnemonic, t); //Insert mnemonic in the symbol table. This mnemonic is the same as the one that is sent as a token.
+                   
+                    p.requestToken(t); //Return token to the Parser
 
-                    //System.out.println(t);
-                    mnemonic = ""; // re-initialize word
+                    mnemonic = ""; // re-initialize word for next loop
                     }
                     
                 }
                 
-                else{
-                    //concatenate characters to string
+                else{   //if i was not equal to ten (ie: not a newline ), concatenate characters to string
                     mnemonic += (char)i;
                     mnemonic = mnemonic.strip();
                 }
         }
 
-        fis.close();
+        fis.close(); //close the FileInputStream object
     }
-    catch(FileNotFoundException e){
+    catch(FileNotFoundException e){ //FileInputStream exception handling, if file was not read
         System.out.println("File not found");
         System.exit(0);
     }
-    catch(IOException e){
+    catch(IOException e){ //Expection handling if error not related to the file
         System.out.println("Error");
         System.exit(0);
     }     
