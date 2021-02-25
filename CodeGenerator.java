54d580dbbe4228bc8e1459cae15a863fd25d55ed
david-gitxyz <79176006+david-gitxyz@users.noreply.github.com>
2021-02-25 00:21:09 -0500

    updated and corrected CodeGenerator

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -1,15 +1,12 @@
 // step 7:  Jay, Vithu, David 
 
-//don't know if we're allowed to use printwriter
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class CodeGenerator {
-    //attributes
-    //private ArrayList<LineStatement> IR = new ArrayList<LineStatement>();
-    //private SymbolTable symbolTable;
+    //Class attributes
     private ArrayList<String> opening = new ArrayList<>(); //array containing all openings of line statement: Line Addr Code
     private ArrayList<String> closing = new ArrayList<>(); //array containing all closings of line statement: Label Mne Operand Comment
     
@@ -17,11 +14,13 @@ public class CodeGenerator {
 
     //methods
     /**
-     * Traverse the IR using symbol table 
+     * Traverse the IR using the symbol table
+     * @param intRep the IR produced
+     * @param symbols the symbol table produced
      */
     public void traverseIR(ArrayList<LineStatement> intRep, SymbolTable symbols){
-        int lineNbr = 1; //line number output
-        int counter = 0; // counter for line number and address
+        int lineNbr = 1; //line number decimal output, will be converted to hexadecimal
+        int counter = 0; // decimal counter for line number and address, will be converted to hexadecimal
         String label = "";
         String mne = "";
         String operand = "";
@@ -31,20 +30,20 @@ public class CodeGenerator {
         String code="";
 
 
-        //for each mnemonic in the table
+        //for each line statement in IR
         for(int i = 0; i < intRep.size(); i++) {
-            //for each line statement in IR
+            //for each symbol in the table
             for(String tableMne : symbols.gHashMap().keySet()) {
                 if(tableMne.equals(intRep.get(i).getMnemonic().getMnemonic())) {    //if table mnemonic is the same as line mnemonic
                     //assign all the info into the appropriate variables
                     //label = intRep.get(i).getLabel().getLabelToken(); //for later
                     mne = intRep.get(i).getMnemonic().getMnemonic();
-                    //operand = intRep.get(i).getMnemonic().getOperand1(); //might change, also this is probably not an int?
+                    //operand = intRep.get(i).getMnemonic().getOperand1(); //for later
                     //comment = intRep.get(i).getComment().getCommentToken(); for later
-                    addr=String.format("%04X", counter); //To hexadecimal (4bit)
+                    addr=String.format("%04X", counter); //Convert decimal counter to hexadecimal (4bit)
                     
                     //find the opcode to the mnemonic
-                   code = String.format("%02X", searchCode(mne)); //To hexadecimal (2bit)
+                   code = String.format("%02X", searchCode(mne)); //Convert opcode to hexadecimal (2bit)
 
                     opening.add(String.format("%-5s%-5s%-6s", lineNbr, addr, code)); 
                     closing.add(String.format("%-10s%-6s%-10s%-10s", label, mne, operand, comment));
@@ -83,25 +82,25 @@ public class CodeGenerator {
         else if(mnemonic.equals("xor"))
             return 15;
         else if(mnemonic.equals("neg"))
-            return 10;
-        else if(mnemonic.equals("inc"))
-            return 11;
-        else if(mnemonic.equals("dec"))
-            return 12;
-        else if(mnemonic.equals("add"))
-            return 13;
-        else if(mnemonic.equals("sub"))
-            return 14; 
-        else if(mnemonic.equals("mul"))
-            return 15; 
-        else if(mnemonic.equals("div"))
             return 16;
-         else if(mnemonic.equals("rem"))
+        else if(mnemonic.equals("inc"))
             return 17;
-        else if(mnemonic.equals("shl"))
+        else if(mnemonic.equals("dec"))
             return 18;
-        else if(mnemonic.equals("shr"))
+        else if(mnemonic.equals("add"))
             return 19;
+        else if(mnemonic.equals("sub"))
+            return 20; 
+        else if(mnemonic.equals("mul"))
+            return 21; 
+        else if(mnemonic.equals("div"))
+            return 22;
+         else if(mnemonic.equals("rem"))
+            return 23;
+        else if(mnemonic.equals("shl"))
+            return 24;
+        else if(mnemonic.equals("shr"))
+            return 25;
         else if(mnemonic.equals("teq"))
             return 26;
         else if(mnemonic.equals("tne"))
@@ -137,7 +136,7 @@ public class CodeGenerator {
         outStr.println(String.format("%-5s%-5s%-6s%-10s%-6s%-10s%-10s", "Line", "Addr", "Code", "Label", "Mne", "Operand", "Comments"));
         System.out.print(String.format("[%-5s%-5s%-6s%-10s%-6s%-10s%-10s]", "Line", "Addr", "Code", "Label", "Mne", "Operand", "Comments")); //for testing purposes
             
-        //Generating opening and closing line statement - TO DO
+        //Generating opening and closing line statement
         for(int i = 0; i < opening.size(); i++) {
             outStr.println(String.format("%-16s%-36s", opening.get(i), closing.get(i)));
             System.out.print(String.format("[%-16s%-36s]", opening.get(i), closing.get(i))); //for testing purposes
