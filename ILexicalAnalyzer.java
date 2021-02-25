e5b6beac65ee2bcae337e3ae467bb8830f2130d2
JoeyMoarbes <78099595+JoeyMoarbes@users.noreply.github.com>
2021-02-25 13:22:32 -0500

    Update ILexicalAnalyzer.java

--- ILexicalAnalyzer.java
+++ ILexicalAnalyzer.java
@@ -1,4 +1,4 @@
-public interface ILexicalAnalyzer{
+public interface ILexicalAnalyzer{ //Interface to be used for the lexicalAnalyzer
     
     public void readFileByLine(Parser p, SymbolTable symbolTable);
     
