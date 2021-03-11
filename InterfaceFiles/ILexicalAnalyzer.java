5c6ee07fd515fdf24a9917a7fdcacb5f21fd063a
david-gitxyz <79176006+david-gitxyz@users.noreply.github.com>
2021-03-11 11:26:18 -0500

    comments from prof
    Comment from prof in lexical analyzer and parser in //COMMENT

---
+++ InterfaceFiles/ILexicalAnalyzer.java
@@ -0,0 +1,11 @@
+package InterfaceFiles;
+
+import SourceFiles.*;
+
+public interface ILexicalAnalyzer { // Interface to be used for the lexicalAnalyzer
+
+    public void readFileByLine(IParser p, ISymbolTable symbolTable); //COMMENT: still dependency problem, constructor injection better, also parser not needed
+
+    public IToken generateToken(); //COMMENT: getToken, nextToken; better naming, should return the interface token
+
+}
