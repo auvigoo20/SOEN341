9370be75fcc2f20d418a25d8b6b85dc60e59a140
Auvigoo Ahmed <auvigooa@gmail.com>
2021-03-24 22:46:29 -0400

    Fixed TestCGenerator

--- InterfaceFiles/ILexicalAnalyzer.java
+++ InterfaceFiles/ILexicalAnalyzer.java
@@ -4,6 +4,8 @@ import SourceFiles.*;
 
 public interface ILexicalAnalyzer { // Interface to be used for the lexicalAnalyzer
 
+    
+    
     public IToken scan();
 
     public IToken getToken();
@@ -11,4 +13,10 @@ public interface ILexicalAnalyzer { // Interface to be used for the lexicalAnaly
     public boolean getFinishScanning();
 
     public void setFinishScanning(boolean fs);
+
+    public ISymbolTable getSymbolTable();
+
+    public void setSymbolTable(ISymbolTable symbolTable);
+
+    
 }
